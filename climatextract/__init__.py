68b7faf84e915d8e4628f80836abc84b6b1d4221
Malte Schierholz <malte.schierholz@gmail.com>
2026-08-28 11:04:50 +0200

    version 0.3.3

--- climatextract/__init__.py
+++ climatextract/__init__.py
@@ -61,6 +61,6 @@ pd.set_option('future.no_silent_downcasting', True)
 
 from climatextract.main import extract, extract_and_evaluate
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __all__ = ["extract", "extract_and_evaluate"]
 
